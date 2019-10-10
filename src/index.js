64c4361a5a9b5c69ffaa96bb8e60463bb42bf932
Pavel Burya <pburya@gmail.com>
2019-10-11 01:03:11 +0300

    init: upload task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, config) {
+    let array = str.split(''),
+        was_cut = false;
+
+    console.log(array);
+
+    while(array.length !== 0) {
+        was_cut = false;
+
+        for (let bracket_pair in config) {
+            if (config.hasOwnProperty(bracket_pair)) {
+
+                console.log(config[bracket_pair]);
+
+                for (let i = 0; i < array.length - 1; i++) {
+                    if (array[i] === config[bracket_pair][0] && array[i + 1] === config[bracket_pair][1]) {
+                        array.splice(i, 2);
+                        i--;
+                        was_cut = true;
+                    }
+                }
+
+            }
+        }
+
+        if (was_cut === false)
+            return false;
+    }
+
+    return true;
+};
